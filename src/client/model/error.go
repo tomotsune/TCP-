9d62cf21e7427eb808f3ae6a892f2d2d284c7cd7
tomotsune <tomotsune@foxmail.com>
2021-09-10 18:23:21 +0800

    v1.6 完成登录

---
+++ src/client/model/error.go
@@ -0,0 +1,7 @@
+package model
+
+import "errors"
+
+var (
+	MEMBER_NOT_EXIST = errors.New("member not exist")
+)
---
+++ src/client/model/member.go
@@ -0,0 +1,6 @@
+package model
+
+type Member struct {
+	mobile string `json:"mobile"`
+	pwd    string `json:"pwd"`
+}
